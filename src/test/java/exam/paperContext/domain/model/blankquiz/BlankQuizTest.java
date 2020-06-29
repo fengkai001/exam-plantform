96931ab0ab326b1d2854321ca157e681d17af096
Kai Feng <kai.feng@thoughtworks.com>
2020-06-29 16:03:12 +0800

    Update Blank Quiz

--- src/test/java/exam/paperContext/domain/model/blankquiz/BlankQuizTest.java
+++ src/test/java/exam/paperContext/domain/model/blankquiz/BlankQuizTest.java
@@ -1,5 +1,6 @@
 package exam.paperContext.domain.model.blankquiz;
 
+
 import exam.blankquizContext.domain.model.blankquiz.BlankQuiz;
 import exam.blankquizContext.domain.model.blankquiz.BlankQuizId;
 import org.junit.jupiter.api.Test;
@@ -11,7 +12,8 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class BlankQuizTest {
 
     @Test
-    public void test_create_blankquiz() {
+    public void test_blankquiz() {
+        // create
         final BlankQuizId blankQuizId = new BlankQuizId("blankquiz-00000");
         final String teacherId = "teacher-11111";
         final String context = "good good study";
@@ -21,5 +23,11 @@ public class BlankQuizTest {
         BlankQuiz blankQuiz = BlankQuiz.create(blankQuizId, teacherId, context, referenceAnswer, score);
         assertThat(blankQuiz, is(notNullValue()));
         System.out.println("create");
+
+        // update
+        final String teacherId2 = "teacher-22222";
+        blankQuiz.revise(teacherId2, context, referenceAnswer, score);
+        assertThat(blankQuiz.getTeacherId(), is(teacherId2));
+        System.out.println("update");
     }
 }
